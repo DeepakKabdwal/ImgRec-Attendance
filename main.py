5e052eff9aaf4a0ba9fd19a7f29174272354ce99
DeepakKabdwal <pikabdwal@gmail.com>
2022-09-20 13:03:48 +0530

    shenanigans

--- main.py
+++ main.py
@@ -18,16 +18,19 @@ for cl in myList:
     images.append(curImg)
     # basically removing the extension from the image it luks gud
     classNames.append(os.path.splitext(cl)[0])
+
+
 # print(classNames)
 # start our encoding process
 
-def enc(images):
+def enc(imagesz):
     encodeList = []
-    for img in images:
+    for img in imagesz:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
 
 encodeListKnown = enc(images)
 print("Done with encoding")
@@ -46,28 +49,23 @@ while True:
     # find encodings of the resized image sending in the small images and The location of
     # all the faces in current frame
     encodesCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
-# one by one it grabs faces from facesCurrFrame list and Encodings From encodesCurrFrame list
+    # one by one it grabs faces from facesCurrFrame list and Encodings From encodesCurrFrame list
     for enc, faceLoc in zip(encodesCurrFrame, facesCurrFrame):
         matches = face_recognition.compare_faces(encodeListKnown, enc)
         # this will return a list which will contain the distance or dis-similaritis the image from camera
         # have and the images provided in the training data
         # the lowest value will have our match
         faceDis = face_recognition.face_distance(encodeListKnown, enc)
-       # print(faceDis)
+        # print(faceDis)
         matchIndex = np.argmin(faceDis)
         if matches[matchIndex]:
             name = classNames[matchIndex]
             print(name)
             y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img,(x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-
-
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
-
-
-
