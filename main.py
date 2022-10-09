3fcf000c6dc139b35917e8c3eff3bc0ceca82986
DeepakKabdwal <pikabdwal@gmail.com>
2022-10-09 15:49:15 +0530

    commented out the imshow line

--- main.py
+++ main.py
@@ -85,5 +85,5 @@ while True:
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
 
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+    # cv2.imshow('Webcam', img)
+    # cv2.waitKey(1)
